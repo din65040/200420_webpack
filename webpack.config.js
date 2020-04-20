fc9a125eb78ca881967b295238b18b8b9a51f316
Shinsuke Ieda <shinsuke.ieda@un-t.com>
2020-04-20 11:17:35 +0900

    webpack のインストール、初期設定

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.export = {
+  mode: 'development',
+  // エントリーポイントの設定
+  entry: './src/js/app.js',
+  output: {
+    // 出力するファイル名
+    filename: 'bundle.js',
+    // 出力先のパス（絶対パスを指定しないとエラーが出るので注意）
+    path: path.resolve(__dirname, 'public/js')
+  }
+}
