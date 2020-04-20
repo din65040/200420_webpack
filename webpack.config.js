28b221f112733fbe467422cc0abd2a0e597ff610
Shinsuke Ieda <shinsuke.ieda@un-t.com>
2020-04-20 23:02:29 +0900

    clean-webpack-plugin の使用

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -9,5 +10,9 @@ module.exports = {
     filename: 'bundle.js',
     // 出力先のパス（絶対パスを指定しないとエラーが出るので注意）
     path: path.resolve(__dirname, 'public/js')
-  }
+  },
+  plugins: [
+    // clean-webpack-plugin の利用
+    new CleanWebpackPlugin()
+  ]
 }
