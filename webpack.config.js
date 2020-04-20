515cb04f7e6aeb54dfed9c8a408b1a9a32cc4037
Shinsuke Ieda <shinsuke.ieda@un-t.com>
2020-04-20 20:06:26 +0900

    SCSS ファイルのコンパイル及びバンドル

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-module.export = {
+module.exports = {
   mode: 'development',
   // エントリーポイントの設定
   entry: './src/js/app.js',
@@ -9,5 +9,25 @@ module.export = {
     filename: 'bundle.js',
     // 出力先のパス（絶対パスを指定しないとエラーが出るので注意）
     path: path.resolve(__dirname, 'public/js')
+  },
+  module: {
+    rules: [
+      {
+        // ローダーの処理対象ファイル
+        test: /\.scss$/,
+        //ローダーの処理対象となるディレクトリ
+        include: path.resolve(__dirname, 'src/scss'),
+        // 利用するローダー
+        // sass-loader css-loader style-loader の順で実行される
+        use: [
+          // HTML に、style-loader で変換した CSS のスタイルが記述された <style> タグを追加する
+          'style-loader',
+          // CSS をモジュールに変換する
+          'css-loader',
+          // Sassを CSS にコンパイルする
+          'sass-loader'
+        ]
+      }
+    ]
   }
 }
