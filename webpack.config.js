cb284662141c19659f2048bfbbbdeaa0a6f4d13a
Shinsuke Ieda <shinsuke.ieda@un-t.com>
2020-04-20 18:33:54 +0900

    babel-loader を追加

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-module.export = {
+module.exports = {
   mode: 'development',
   // エントリーポイントの設定
   entry: './src/js/app.js',
@@ -9,5 +9,27 @@ module.export = {
     filename: 'bundle.js',
     // 出力先のパス（絶対パスを指定しないとエラーが出るので注意）
     path: path.resolve(__dirname, 'public/js')
+  },
+  module: {
+    rules: [
+      {
+        // ローダーの処理対象ファイル
+        test: /\.js$/,
+        // ローダーの処理対象となりディレクトリ
+        include: path.resolve(__dirname, 'src/js'),
+        use: [
+          {
+            // 使用するローダー
+            loader: 'babel-loader',
+            // ローダーのオプション
+            options: {
+              presets: [
+                ['@babel/preset-env', { modules: false }]
+              ]
+            }
+          }
+        ]
+      }
+    ]
   }
 }
