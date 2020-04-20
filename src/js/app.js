c95f9f8d9c038a7e6af9fc35690d461f1e6d1f5d
Shinsuke Ieda <shinsuke.ieda@un-t.com>
2020-04-20 22:06:54 +0900

    ProvidePlugin の使用

--- src/js/app.js
+++ src/js/app.js
@@ -1,23 +1,3 @@
-import $ from 'jquery';
-import add from './modules/add';
-import tax from './modules/tax';
+// import $ from 'jquery';
 
-const item1Price = 400;
-const item2Price = 600;
-const totalPrice = add(item1Price, item2Price);
-const salesTax = 1.08;
-const priceIncludeTax = tax(totalPrice, salesTax);
-
-$('body').text(priceIncludeTax);
-
-console.log('console');
-
-// ---
-// import {add, sub as SUB} from './math.js'
-// add(1,2);
-// SUB(3,2);
-
-// ---
-// import * as math from './math.js'
-// math.add(1,2);
-// math.sub(3,2);
+$('body').html('Hello webpack');
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
@@ -9,5 +10,13 @@ module.exports = {
     filename: 'bundle.js',
     // 出力先のパス（絶対パスを指定しないとエラーが出るので注意）
     path: path.resolve(__dirname, 'public/js')
-  }
+  },
+  // プラグインの設定
+  plugins: [
+    // ProvidePlugin を利用する
+    new webpack.ProvidePlugin({
+      // 外部モジュールである jquery を、全てのファイル上で変数 $ として利用できるようにする
+      $: 'jquery'
+    })
+  ]
 }
