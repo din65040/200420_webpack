c962f6351b15dd4fcf435e2178791fced721dbcc
Shinsuke Ieda <shinsuke.ieda@un-t.com>
2020-04-20 20:27:08 +0900

    url-loader の使用

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-module.export = {
+module.exports = {
   mode: 'development',
   // エントリーポイントの設定
   entry: './src/js/app.js',
@@ -9,5 +9,29 @@ module.export = {
     filename: 'bundle.js',
     // 出力先のパス（絶対パスを指定しないとエラーが出るので注意）
     path: path.resolve(__dirname, 'public/js')
+  },
+  module: {
+    rules: [
+      {
+        // ローダーの処理対象ファイル
+        test: /\.scss$/,
+        // ローダーの処理対象となるディレクトリ
+        include: path.resolve(__dirname, 'src/scss'),
+        // 利用するローダー
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        // ローダーの処理対象ファイル
+        test: /\.(png|jpg|gif)$/i,
+        // ローダーの処理対象となるディレクトリ
+        include: path.resolve(__dirname, 'src/images'),
+        // 利用するローダー
+        loader: 'url-loader'
+      }
+    ]
   }
 }
